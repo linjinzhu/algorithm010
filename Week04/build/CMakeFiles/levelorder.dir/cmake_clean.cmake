45283426a3210e2a61e77852a6452aa2411a5d03
linjinzhu <linjinzhu@baidu.com>
2020-07-05 22:51:42 +0800

    Week04

---
+++ Week04/build/CMakeFiles/levelorder.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/levelorder.dir/level_traversal.cpp.o"
+  "levelorder.pdb"
+  "levelorder"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/levelorder.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Week04/build/CMakeFiles/numofislands.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/numofislands.dir/num_of_islands.cpp.o"
+  "numofislands.pdb"
+  "numofislands"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/numofislands.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
